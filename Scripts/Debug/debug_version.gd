355308cd25f06cb9a8a233d540c61efc4c4bfdb8
Arthur Perret <aperret8@univ-lyon2.fr>
2025-01-29 11:40:44 +0100

    Interface Debug

--- Scripts/Debug/debug_version.gd
+++ Scripts/Debug/debug_version.gd
@@ -1,9 +1,31 @@
 extends Control
 
+var dictionnaire : Dictionary = {
+	0 : "Debug",
+	1 : "Debug2"
+	}
+	
+var current_map : String = "Debug"
+
 @onready var ver_button = $Versioning
+@onready var line_edit = $LineEdit
 
 func _ready() -> void:
 	ver_button.text = ProjectSettings.get_setting("application/config/version")
 
 func _on_button_pressed() -> void:
 	DisplayServer.clipboard_set(ProjectSettings.get_setting("application/config/version"))
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	ThreadLoad.load_scene(Game.level_container, dictionnaire[index], 0)
+	current_map = dictionnaire[index]
+
+
+func _on_line_edit_text_submitted(new_score: String) -> void:
+	Game.ui.update_score(int(new_score))
+	line_edit.text = ""
+
+
+func _on_respawn_debug_item_selected(index: int) -> void:
+	ThreadLoad.load_scene(Game.level_container, current_map, index)
