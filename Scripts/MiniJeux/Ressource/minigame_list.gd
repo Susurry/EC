cd56b4550df6db4ef2cb2f307e1332d60796ccb7
Arthur Perret <aperret8@univ-lyon2.fr>
2025-02-26 15:54:57 +0100

    Minigame Setup

---
+++ Scripts/MiniJeux/Ressource/minigame_list.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name MinigameList
+
+@export var dictionnary_minigame : Dictionary
---
+++ Scripts/MiniJeux/mini_game_manager.gd
@@ -0,0 +1,26 @@
+extends SubViewport
+
+@export var list_minigame :Resource
+
+var minigame_display: PackedScene
+
+@onready var game = get_parent().get_parent().viewport
+
+func _ready() -> void:
+	EventBus.add_signal("setup_minigame", setup_minigame)
+
+func setup_minigame(minigame_name: String) -> void:
+	var minigame_load = load(list_minigame.dictionnary_minigame[minigame_name])
+	var minigame_instance: Node2D = minigame_load.instantiate()
+	add_child(minigame_instance)
+	get_parent().visible = true
+	game.get_tree().paused = true
+	
+func erase_minigame() -> void:
+	for n in self.get_children():
+		self.remove_child(n)
+		n.queue_free()
+		
+	get_parent().visible = false
+	game.get_tree().paused = false
+	
