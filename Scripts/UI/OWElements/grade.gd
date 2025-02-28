27d436729f15cc306d898e7ecea454e2eb4dcfb6
Arthur Perret <aperret8@univ-lyon2.fr>
2025-02-28 16:05:14 +0100

    minor fix

--- Scripts/UI/OWElements/grade.gd
+++ Scripts/UI/OWElements/grade.gd
@@ -10,22 +10,22 @@ func update_grading() -> void:
 	match grading_score:
 		var x when x >= 18:
 			grade_label.text = "F"
-			timer_element.time_multi = 0.75
+			timer_element.time_multi = 1.5
 		var x when x < 18 and x >= 15:
 			grade_label.text = "E"
-			timer_element.time_multi = 0.8571
+			timer_element.time_multi = 1.5
 		var x when x < 15 and x >= 12:
 			grade_label.text = "D"
-			timer_element.time_multi = 0.8571
+			timer_element.time_multi = 1.2
 		var x when x < 12 and x >= 9:
 			grade_label.text = "C"
 			timer_element.time_multi = 1
 		var x when x < 9 and x >= 6:
 			grade_label.text = "B"
-			timer_element.time_multi = 1.2
+			timer_element.time_multi = 0.8571
 		var x when x < 6 and x > 2 :
 			grade_label.text = "A"
-			timer_element.time_multi = 1.2
+			timer_element.time_multi = 0.75
 		var x when x <= 2:
 			grade_label.text = "S"
-			timer_element.time_multi = 1.5
+			timer_element.time_multi =  0.75
