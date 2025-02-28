89fa0d3759f8af6b6c21be193ba9ac2d0d157f91
Arthur Perret <aperret8@univ-lyon2.fr>
2025-02-28 14:03:15 +0100

    small fix

--- Scripts/MiniJeux/default.gd
+++ Scripts/MiniJeux/default.gd
@@ -7,22 +7,20 @@ var bath_full: bool = false
 
 @onready var animated_sprite = $WindowSize/AnimatedSprite2D
 
-func _process(float) -> void:
+func _process(delta: float) -> void:
 	if animated_sprite.is_playing() == false && bath_full == true :
 			animated_sprite.play("shower")
 			shower = true
 			bath_full = false
 			
 
-func _on_button_pressed(arg: String) -> void:
-	if arg == "play_anim":
-		if shower == false :
-			animated_sprite.play("start_shower")
-			bath_full = true
-		if shower == true :
-			animated_sprite.play("shower_end")
-			shower = false
-			return
+func _on_button_pressed() -> void:
+	if shower == false :
+		animated_sprite.play("start_shower")
+		bath_full = true
+	if shower == true :
+		animated_sprite.play("shower_end")
+		shower = false
 
 
 func _on_bouton_quitter_pressed() -> void:
