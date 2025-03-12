b2b606fcaa9624511702dc0856d6e7fa011c773a
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-03-12 12:25:51 +0100

    Upgrade Map Debug Tool + Player Sit Bug + Tweaks
    - Map Debug Tool automatically has the maps listed
    - Fixed Player being in Sit state by default

--- Scripts/Debug/debug_version.gd
+++ Scripts/Debug/debug_version.gd
@@ -1,25 +1,30 @@
 extends Control
 
-var dictionnaire : Dictionary[int,String] = {
-	0 : "Debug",
-	1 : "Debug2",
-	2 : "Appartement",
-	3 : "PeriphOuest",
-	4 : "CentreVille"
-}
+@onready var maps: Resource = preload("uid://byrvrkqe18ml0")
 
-var current_map : String = "Debug"
+var map_list: Dictionary[int,String]
+
+var current_map : String = ""
 
 @onready var ver_button = $Rows/Versioning
 @onready var line_edit = $Rows/LineEdit
+@onready var map_options = $Rows/MapDebug
 
 func _ready() -> void:
 	initialize_debug_tools()
+	initialize_map_list()
 	intialize_versionning()
 
 func initialize_debug_tools() -> void:
 	if !OS.is_debug_build():
 		queue_free()
+
+func initialize_map_list() -> void:
+	var num: int = 0
+	for key in maps.locations.keys():
+		map_list[num] = key
+		map_options.add_item(key, num)
+		num += 1
 
 func intialize_versionning() -> void:
 	ver_button.text = ProjectSettings.get_setting("application/config/version")
@@ -35,8 +40,8 @@ func _on_line_edit_text_submitted(new_score: String) -> void:
 
 # SCENE TELEPORTER
 func _on_option_button_item_selected(index: int) -> void:
-	ThreadLoad.load_scene(dictionnaire[index])
-	current_map = dictionnaire[index]
+	ThreadLoad.load_scene(map_list[index])
+	current_map = map_list[index]
 
 func _on_respawn_debug_item_selected(index: int) -> void:
 	ThreadLoad.load_scene(current_map, index)
