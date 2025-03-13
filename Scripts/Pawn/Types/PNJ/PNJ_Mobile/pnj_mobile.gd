2fadf49acc4e99caa4360696f97819d2e474de6c
Arthur Perret <aperret8@univ-lyon2.fr>
2025-03-13 10:50:49 +0100

    PNJ Mobile Random

--- Scripts/Pawn/Types/PNJ/PNJ_Mobile/pnj_mobile.gd
+++ Scripts/Pawn/Types/PNJ/PNJ_Mobile/pnj_mobile.gd
@@ -1,24 +1,38 @@
 extends PNJ
 class_name PNJMobile
 
-@export var targets: Node2D
+@export var middle_targets: Node2D
+@export var spawn_targets: Node2D
 
-var target_array: Array
+var spawn_point: Node2D
+var middle_target_array: Array
+var spawn_target_array: Array
 var target_id: int = 0
 
+var targets: Array[Vector2]
+
 func initialize_pawn():
-	# Wait for the first physics frame so the NavigationServer can sync.
 	await get_tree().physics_frame
-	target_array = targets.get_children()
-	# Now that the navigation map is no longer empty, set the movement target.
-	set_movement_target(target_array[target_id].position)
-	#Pensez à connecter les signaux !
+	middle_target_array = middle_targets.get_children()
+	spawn_target_array = spawn_targets.get_children()
+	
+	randomize()
+	
+	var last_target: int
+	while targets.size() < 3:
+		var rng_middle_target: int = randi_range(0, 2)
+		if rng_middle_target != last_target:
+			last_target = rng_middle_target
+			targets.append(middle_target_array[rng_middle_target].position)
+	
+	var rng_end_target: int = randi_range(0, 1)
+	targets.append(spawn_target_array[rng_end_target].position)
+	
+	set_movement_target(targets[target_id])
 
 func _on_navigation_agent_2d_navigation_finished() -> void:
-	if target_id >= target_array.size() - 1:
+	target_id += 1
+	if target_id >= targets.size():
 		queue_free()
-	else:	
-		target_id += 1
-		set_movement_target(target_array[target_id].position)
-	
-	#Faire le PNJ Manager.
+	else:
+		set_movement_target(targets[target_id])
