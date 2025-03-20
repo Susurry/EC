f1da13b1b775c011b895d561693808ee9dbea014
Arthur Perret <arthur.perret@univ-lyon2.fr>
2025-03-20 16:02:51 +0100

    MiniJeu Baignoire

--- Scripts/MiniJeux/Jeux/baignoire.gd
+++ Scripts/MiniJeux/Jeux/baignoire.gd
@@ -1,20 +1,67 @@
 extends Control
 
-var shower: bool = false
-var bath_full: bool = false
+var shower_completed: bool = false
+var bouchon_on: bool = false
+var stop: bool = false
 
 @onready var animated_sprite = $SpriteBaignoire
+@onready var sprite_bouchon = $Bouchon
+@onready var valve = $SpriteBaignoire/ValveDroite
 
-func _process(_delta: float) -> void:
-	if animated_sprite.is_playing() == false && bath_full == true :
-			animated_sprite.play("shower")
-			shower = true
-			bath_full = false
+@onready var timer = $Timer
+@onready var bar: ProgressBar = $ProgressBar
+var shower_paused: bool = true
+
+func shower_start() -> void:
+	sprite_bouchon.visible = false
+	animated_sprite.play("start_shower")
+	await animated_sprite.animation_finished
+	shower_paused = false
+	timer.start()
+
+func shower():
+	bar.value += 1
+	
+	if bouchon_on:
+		if bar.value >= 6:
+			shower_end()
+	else:
+		if bar.value >= 6 and bar.value < 12:
+			print("score -2")
+		elif bar.value >= 12 and bar.value < 18:
+			print("score +1")
+		elif bar.value >= 18:
+			print("score +2")
+			shower_end()
+			return
+		
+		valve.disabled = false
+
+func shower_end() -> void:
+	timer.stop()
+	valve.disabled = true
+	animated_sprite.play("shower_end")
+	await get_tree().create_timer(2.0).timeout
+	print("Fin")
+	#get_parent().quit_minigame()
+
+func shower_stop():
+	if bar.value >= 6:
+		shower_end()
+		return
+
+	animated_sprite.play("shower_end")
+	await animated_sprite.animation_finished
+	shower_paused = true
+	timer.stop()
+	valve.disabled = false
 
 func _on_button_pressed() -> void:
-	if shower == false :
-		animated_sprite.play("start_shower")
-		bath_full = true
-	if shower == true :
-		animated_sprite.play("shower_end")
-		shower = false
+	valve.disabled = true
+	if shower_paused:
+		shower_start()
+	else:
+		shower_stop()
+
+func _on_bouchon_pressed() -> void:
+	bouchon_on = !bouchon_on
