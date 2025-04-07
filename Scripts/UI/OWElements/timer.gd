e92a58a4fe3fd0998a52b778f3fc0730afcfd162
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-04-07 10:01:31 +0200

    Add Timer Bar Animation Tween

--- Scripts/UI/OWElements/timer.gd
+++ Scripts/UI/OWElements/timer.gd
@@ -8,6 +8,7 @@ extends Control
 var time : float
 var format_string = "%02d : %02d : %02d"
 var tween: Tween
+var is_tweening: bool
 
 @onready var time_label = $PanelLabel/Label
 @onready var timer_bar = $BarreTemps/PanelProgressBar/TimeBar
@@ -33,12 +34,27 @@ func _process(delta: float) -> void:
 		seconde = int(time) % 60
 		time_label.text = format_string % [heure, minute, seconde]
 		
-		timer_bar.value = -time
+		if not is_tweening:
+			timer_bar.value = -time
 	else:
 		pass
 
 func change_time(var_time: float) -> void:
+	is_tweening = true
 	time += var_time
+	
+	if var_time < 0:
+		changing_timer_bar.visible = true
+		changing_timer_bar.value = -time
+		
+	tween = create_tween()
+	tween.connect("finished", tween.kill)
+	tween.tween_property(timer_bar, "value", -time, 0.5)
+	
+	await tween.finished
+	
+	is_tweening = false
+	changing_timer_bar.visible = false
 
 func _on_button_pressed() -> void:
 	change_time(1000.0)
