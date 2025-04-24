393fef80d8b544e715c840e0395eded2c4fa5eed
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-04-24 15:28:07 +0200

    Fix bug sortie de l'appart/ entrer du périph

--- Scripts/Events/PeriphOuest/periph_save_checks.gd
+++ Scripts/Events/PeriphOuest/periph_save_checks.gd
@@ -5,6 +5,7 @@ extends Node2D
 func initialize() -> void:
 	if SaveManager.getElement("Events", timeline) == null:
 		SaveManager.setElement("Events", {timeline: false})
+		await Dialogic.timeline_ended
 		Dialogic.start(timeline)
 	
 	$MissionEboueur.check_mission_done()
