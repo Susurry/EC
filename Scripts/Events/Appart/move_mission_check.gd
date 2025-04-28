29863510a2fe24e3e6595dd78dcb0a960f15f65c
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-04-28 10:45:31 +0200

    Tutorial Chair behaviour change

--- Scripts/Events/Appart/move_mission_check.gd
+++ Scripts/Events/Appart/move_mission_check.gd
@@ -19,7 +19,7 @@ func _physics_process(_delta: float) -> void:
 func check_mission_done() -> void:
 	if SaveManager.getElement("Quests", "0-1_deplacer") == false:
 		# Lorsque la mission est accomplie
-		Dialogic.start("Introduction", "book4")
+		Dialogic.start("intro_appart", "book4")
 	else:
 		# Supprime la mission si elle est déjà accomplie
 		queue_free()
