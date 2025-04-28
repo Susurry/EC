cdcf2b8f4f9b831a5b628baae784fa365a527289
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-04-28 13:36:10 +0200

    Save pour Score et Follower + Player Car position

--- Scripts/Events/Shared/player_car_check.gd
+++ Scripts/Events/Shared/player_car_check.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@export var current_location: String
+@export var car_object: Node2D
+
+func check_state() -> void:
+	if SaveManager.getElement("Misc", "Car") == null:
+		SaveManager.setElement("Misc", {"Car": "PeriphOuest"})
+	else:
+		if not SaveManager.getElement("Misc", "Car") == current_location:
+			car_object.queue_free()
