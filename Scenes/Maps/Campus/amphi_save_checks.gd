800be14fecd759aa8723787cade70eef257c92d8
Lemosquet Emilien <elemosquet@gmail.com>
2025-04-29 11:34:23 +0200

    Cinématique Fin, Réécriture et Characters
    - Cinématique Amphi (Save Checks)
    - Characters pour tous les persos du jeu
    - Réécritures certains dialogues WIP
    - Terminer le jeu avec le Vélo
    - Brrr, skibidi bop bop dob yes yes

---
+++ Scenes/Maps/Campus/amphi_save_checks.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@export var timeline: String
+
+func initialize() -> void:
+	if SaveManager.getElement("Events", timeline) == null:
+		SaveManager.setElement("Events", {timeline: false})
+		Dialogic.start(timeline)
