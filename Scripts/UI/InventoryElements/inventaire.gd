f2beb605e2ade2515d710b9ef18ed8661533b319
ArthurCottreau <arthur.cottreau@univ-lyon2.fr>
2025-05-01 14:39:49 +0200

    Swap tracks

--- Scripts/UI/InventoryElements/inventaire.gd
+++ Scripts/UI/InventoryElements/inventaire.gd
@@ -32,6 +32,12 @@ func add_item(item_name: String) -> void:
 	item_load.name = item_name
 	item_load.get_node("MarginContainer/TextureRect").texture = load(list_item.dictionnaire[item_name])
 	
+	if SaveManager.getElement("Misc", "first_time_added") == null:
+		SaveManager.setElement("Misc", {"first_time_added": true})
+		if Dialogic.current_timeline:
+			await Dialogic.timeline_ended
+		Dialogic.start("intro_appart", "book14")
+	
 	item_column.add_child(item_load)
 	item_list.append(item_name)
 	visible = check_amount()
